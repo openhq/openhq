e80b5404008a82ba4a9bdb7f12a0cd0017ad9e54
Pete Hawkins <pete@phawk.co.uk>
2015-02-06 22:00:43 +0000

    Base css/js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require modernizr
 //= require jquery
 //= require jquery_ujs
 //= require jquery.timeago
