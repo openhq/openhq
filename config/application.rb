a0116df3de6b056c08c4833b232675eec402186d
Pete Hawkins <pete@phawk.co.uk>
2015-02-06 19:52:06 +0000

    Some default settings for generators and time zone

--- config/application.rb
+++ config/application.rb
@@ -8,16 +8,27 @@ Bundler.require(*Rails.groups)
 
 module ProjectManagementApp
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'London'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.active_job.queue_adapter = :sucker_punch
+
+    config.generators do |g|
+      g.test_framework :minitest, spec: true, fixture: false
+      g.skip_routes true
+      g.controller_specs false
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+    end
+
+    # Custom sign-in layout
+    # config.to_prepare do
+    #   Devise::SessionsController.layout "auth"
+    #   Devise::ConfirmationsController.layout "auth"
+    #   Devise::UnlocksController.layout "auth"
+    #   Devise::PasswordsController.layout "auth"
+    # end
   end
 end
