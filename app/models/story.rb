5c3113fb491cb186d4750cef1bed377d99276869
Pete Hawkins <pete@phawk.co.uk>
2015-02-16 10:35:54 +0000

    updates

--- app/models/story.rb
+++ app/models/story.rb
@@ -5,7 +5,7 @@ class Story < ActiveRecord::Base
   belongs_to :project
   belongs_to :owner, class_name: "User"
   has_many :tasks
-  has_many :attachments, as: :attachable
+  has_many :attachments
   has_many :comments, as: :commentable
 
   validates_presence_of :project_id, :name, :owner_id
