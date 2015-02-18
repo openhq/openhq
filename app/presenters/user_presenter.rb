d8d3389d50e9aaf00093c09842ab9d6561b0e04a
Pete Hawkins <pete@phawk.co.uk>
2015-02-18 21:52:39 +0000

    Use gravatar for initial avatars and begin project list styles

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -1,8 +1,34 @@
+require 'digest/md5'
+
 class UserPresenter < BasePresenter
   presents :user
 
   def full_name
     "#{user.first_name} #{user.last_name}"
+  end
+
+  def avatar_link(size = 100)
+    # TODO implement link to user
+    h.link_to avatar(size), "#"
+  end
+
+  def avatar(size = 100)
+    # TODO implement uploadable avatars to override gravatar if exists
+
+    url = gravatar_url(size)
+
+    h.image_tag(url, alt: full_name, class: "avatar")
+  end
+
+  private
+
+  def gravatar_url(size = 200)
+    base_url = "//www.gravatar.com/avatar/"
+    opts = "?s="
+
+    hash = Digest::MD5.hexdigest(user.email)
+
+    base_url + hash + opts + size.to_s
   end
 
 end
