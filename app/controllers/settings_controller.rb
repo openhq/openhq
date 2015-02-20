171dc9ffd3794faad02265a013ca8e00f53aaf34
Pete Hawkins <pete@phawk.co.uk>
2015-02-20 22:09:08 +0000

    Updaet authorization for settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,10 +1,12 @@
 class SettingsController < ApplicationController
   def edit
     @settings = Settings.load
+    authorize!(:update, @settings)
   end
 
   def update
     @settings = Settings.load
+    authorize!(:update, @settings)
 
     if @settings.update(settings_params)
       redirect_to root_path, notice: "Your settings have been updated"
