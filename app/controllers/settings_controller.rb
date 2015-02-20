ca86e2bee4995a9507db7f11ff7b4496d2387d90
Pete Hawkins <pete@phawk.co.uk>
2015-02-20 22:05:34 +0000

    Add overall settings page

---
+++ app/controllers/settings_controller.rb
@@ -0,0 +1,21 @@
+class SettingsController < ApplicationController
+  def edit
+    @settings = Settings.load
+  end
+
+  def update
+    @settings = Settings.load
+
+    if @settings.update(settings_params)
+      redirect_to root_path, notice: "Your settings have been updated"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def settings_params
+    params.require(:settings).permit(:organisation_name)
+  end
+end
