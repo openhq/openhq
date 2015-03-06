356cddd3e75a0eb7fdff11d96064d00f2b2b19fc
Colin Young <hello@colin-young.co.uk>
2015-03-06 17:44:37 +0000

    test fix

--- test/factories/attachment.rb
+++ test/factories/attachment.rb
@@ -3,9 +3,9 @@ require 'faker'
 FactoryGirl.define do
   factory :attachment do
     name { Faker::Lorem.word }
-    path { Faker::Lorem.word }
-    size { Faker::Number.number(6) }
-    content_type { Faker::Lorem.word }
+    attachment_file_name "test.jpg"
+    attachment_file_size "1234"
+    attachment_content_type "image/jpeg"
     association :owner, factory: :user
   end
 end
