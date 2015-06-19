fe2ed4a9744b5df62f07267362cbcb57011041e7
Pete Hawkins <pete@phawk.co.uk>
2015-06-19 20:39:45 +0100

    don’t use fetch in initializer so we don’t need env vars on codeship

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,7 +2,7 @@ Paperclip::Attachment.default_options[:storage] = :s3
 Paperclip::Attachment.default_options[:s3_host_name] = ENV['AWS_S3_HOST'] || nil
 Paperclip::Attachment.default_options[:s3_protocol] = :https
 Paperclip::Attachment.default_options[:s3_credentials] = {
-  bucket: ENV.fetch('AWS_S3_BUCKET'),
-  access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-  secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
+  bucket: ENV['AWS_S3_BUCKET'],
+  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
 }
