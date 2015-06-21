329631d2a17a5f17fdb85a63ef3cec604f09a90e
Pete Hawkins <pete@phawk.co.uk>
2015-06-21 16:23:21 +0100

    opt in to new functionality and remove deprecation warning

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module ProjectManagementApp
 
     config.active_job.queue_adapter = :sucker_punch
 
+    config.active_record.raise_in_transactional_callbacks = true
+
     config.generators do |g|
       g.test_framework :minitest, spec: true, fixture: false
       g.skip_routes true
