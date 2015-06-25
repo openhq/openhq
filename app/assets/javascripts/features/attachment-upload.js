6b5ab4a13218e0f9b356b255d4f700175278dc09
Pete Hawkins <pete@phawk.co.uk>
2015-06-25 16:08:14 +0100

    Disable form button when uploading

--- app/assets/javascripts/features/attachment-upload.js
+++ app/assets/javascripts/features/attachment-upload.js
@@ -1,4 +1,4 @@
-$(function(){
+App.onPageLoad(function() {
 
   $('#s3-uploader').S3Uploader({
     allow_multiple_files: true,
@@ -9,6 +9,24 @@ $(function(){
     progress_bar_target: $('#attachments-list'),
     remove_completed_progress_bar: false
   });
+
+  $("#s3-uploader").on("s3_uploads_start", function(e) {
+    // An upload has started disable form
+    var $target = $($(this).attr("data-target-form"));
+
+    $target.find('input[type="submit"]').prop("disabled", true);
+  });
+
+  $("#s3-uploader").on("s3_uploads_complete", function(e) {
+    // All uploads complete re-enable form
+    var $target = $($(this).attr("data-target-form"));
+
+    $target.find('input[type="submit"]').prop("disabled", false);
+  });
+
+  // $("#s3-uploader").on("s3_upload_failed", function(e) {
+    // An upload has failed
+  // });
 
   $("#s3-uploader").on("s3_upload_complete", function(e, content) {
     var $progress_bar = $("#file-"+ content.unique_id);
