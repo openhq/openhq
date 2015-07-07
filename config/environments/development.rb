11c8669f98a53fb7ce4090c8a77cde35572db0eb
Pete Hawkins <pete@phawk.co.uk>
2015-07-07 23:15:21 +0100

    Speed up asset loading in development mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,10 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
+  config.assets.compress = false
+  config.assets.digest = true
+  config.assets.compile = true
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
