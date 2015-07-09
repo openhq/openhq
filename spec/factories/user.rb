f10c52ace71ff4c5edf6ff44f0da0bb3810f450a
Pete Hawkins <pete@phawk.co.uk>
2015-07-09 17:29:27 +0100

    Fix dupe username issues

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,26 +1,13 @@
 require 'faker'
-require 'set'
 
 FactoryGirl.define do
   factory :user do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    username { unique_username }
+    sequence {|n| "user_#{1}" }
     email { Faker::Internet.email }
     password "testpassword1"
     password_confirmation "testpassword1"
     role { ["user", "admin", "owner"].sample }
   end
-end
-
-$taken_usernames = Set.new
-
-def unique_username
-  username = nil
-  loop do
-    username = Faker::Internet.user_name(nil, %w[-_]).downcase[0..17]
-    next if $taken_usernames.include?(username)
-    break
-  end
-  username
 end
