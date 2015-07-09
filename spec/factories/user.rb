db37ef042f4767c3f7539c293b7d64f3342c5950
Pete Hawkins <pete@phawk.co.uk>
2015-07-09 17:30:47 +0100

    finally

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :user do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    sequence {|n| "user_#{1}" }
+    sequence(:username) {|n| "fake_user_#{n}" }
     email { Faker::Internet.email }
     password "testpassword1"
     password_confirmation "testpassword1"
