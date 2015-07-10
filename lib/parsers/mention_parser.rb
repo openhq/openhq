c9e7e6a4e093cbe2f15c0619d6339adfcdddf7f5
Colin Young <hello@colin-young.co.uk>
2015-07-10 16:51:56 +0100

    improve the mentions to ignore the commas after a username

--- lib/parsers/mention_parser.rb
+++ lib/parsers/mention_parser.rb
@@ -2,7 +2,7 @@ class MentionParser
   def self.users(content)
     return unless content.present?
 
-    usernames = content.scan(/@\S+/).map { |u| u.gsub(/@/, '') }
+    usernames = content.scan(/\B@\w+/).map { |u| u.gsub(/@/, '') }
     User.where("username IN (?)", usernames) if usernames.any?
   end
 end
