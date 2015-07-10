65443e25ad10de5c2a87304328721f7d5de3942d
Colin Young <hello@colin-young.co.uk>
2015-07-10 16:56:45 +0100

    adding a couple more tests

--- spec/lib/parsers/mention_parser_spec.rb
+++ spec/lib/parsers/mention_parser_spec.rb
@@ -6,12 +6,26 @@ RSpec.describe MentionParser do
   let!(:mac) { create(:user, username: "mac") }
   let!(:charlie) { create(:user, username: "charlie") }
 
+  it "returns nil if string not passed through" do
+    mentioned = MentionParser.users("")
+
+    expect(mentioned).to equal(nil)
+  end
+
   it "only finds the at mentioned users" do
     string = "@mac and @charlie, you've been at mentioned, but frank hasn't."
     mentioned = MentionParser.users(string)
 
+    expect(mentioned.count).to equal(2)
     expect(mentioned).to include(mac)
     expect(mentioned).to include(charlie)
     expect(mentioned).not_to include(frank)
+  end
+
+  it "returns nil if no one has been at mentioned" do
+    string = "mac, charlie and frank have not been at mentioned"
+    mentioned = MentionParser.users(string)
+
+    expect(mentioned).to equal(nil)
   end
 end
