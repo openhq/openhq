81acec4c29ca148fd9e8ee1dab14d94f8b03de73
Pete Hawkins <pete@phawk.co.uk>
2015-07-14 18:35:39 +0100

    Add some more tests for attachments

--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -3,7 +3,23 @@ require 'rails_helper'
 RSpec.describe Attachment do
   let(:attachment) { build(:attachment) }
 
+  it { should belong_to(:attachable) }
+  it { should belong_to(:story) }
+  it { should belong_to(:owner) }
+
+  it { should validate_presence_of(:owner_id) }
+
   it "must be valid" do
     expect(attachment).to be_valid
+  end
+
+  describe "#extension" do
+    let(:image) { build(:attachment, file_name: "white-russian.JPG") }
+    let(:video) { build(:attachment, file_name: "film.mov") }
+
+    it "returns the file extension" do
+      expect(image.extension).to eq("jpg")
+      expect(video.extension).to eq("mov")
+    end
   end
 end
