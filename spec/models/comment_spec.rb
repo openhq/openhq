c87489fd453866edb9f78dcdb019f64fa9260f66
Pete Hawkins <pete@phawk.co.uk>
2015-07-14 18:47:45 +0100

    Add some specs for Comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,6 +3,13 @@ require 'rails_helper'
 RSpec.describe Comment do
   let(:comment) { build(:comment) }
 
+  it { should belong_to(:commentable) }
+  it { should belong_to(:owner).class_name("User") }
+  it { should have_many(:attachments) }
+
+  it { should validate_presence_of(:content) }
+  it { should validate_presence_of(:owner_id) }
+
   it "must be valid" do
     expect(comment).to be_valid
   end
