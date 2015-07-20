c563d711d7d37d56534a66d804ee454366ce178f
Pete Hawkins <pete@phawk.co.uk>
2015-07-20 12:33:28 +0100

    Fix line length metric

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,9 @@ class User < ActiveRecord::Base
       case_sensitive: false
     }
 
-  scope :active, -> { not_deleted.where("users.invitation_created_at IS NULL OR users.invitation_accepted_at IS NOT NULL") }
+  scope :active, lambda {
+    not_deleted.where("users.invitation_created_at IS NULL OR users.invitation_accepted_at IS NOT NULL")
+  }
   scope :not_deleted, -> { where("deleted_at IS NULL") }
 
   # Overide devise finder to lookup by username or email
