9d55317c5140f2440d7843bf07109d6ce66f35ff
Colin Young <hello@colin-young.co.uk>
2015-07-26 21:56:12 +0100

    use the tile image if it is available

--- app/presenters/attachment_presenter.rb
+++ app/presenters/attachment_presenter.rb
@@ -1,6 +1,10 @@
 class AttachmentPresenter < BasePresenter
   presents :attachment
 
+  def thumbnail(size)
+    attachment.process_data["thumbnail:#{size.to_s}"]
+  end
+
   def icon_name
     case attachment.extension
     when "css", "html", "js", "doc", "mkv", "mp3", "pdf", "png", "jpg", "gif", "psd", "rar", "zip", "txt"
