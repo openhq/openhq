d78f2d50d3e0dd0295974f36f4b585235574454a
Colin Young <hello@colin-young.co.uk>
2015-07-30 14:30:46 +0100

    track any failed process attempts and store a processed at datetime

--- app/jobs/process_attachment_job.rb
+++ app/jobs/process_attachment_job.rb
@@ -2,9 +2,23 @@ require "attachment_processors/image"
 
 class ProcessAttachmentJob < ActiveJob::Base
   queue_as :default
+  MAX_ATTEMPTS = 5
 
   def perform(attachment)
     processor_for(attachment).process(attachment)
+
+    attachment.processed_at = Time.zone.now
+    attachment.save!
+
+  # If the processing fails increment the process attempts
+  #
+  # if it fails the max number of times, do not raise the exception.
+  # This will stop sidekiq from placing it in the retries pile
+  rescue StandardError => e
+    attachment.process_attempts += 1
+    attachment.save
+
+    raise e if attachment.process_attempts < MAX_ATTEMPTS
   end
 
   private
