65eccf9c14eea5afd255e4333b3d5f9c0d38024c
Colin Young <hello@colin-young.co.uk>
2015-07-31 14:50:35 +0100

    use js to scroll to and add class warmdown to any selected elements through the url hash

---
+++ app/assets/javascripts/features/search.js
@@ -0,0 +1,34 @@
+$(function(){
+    App.onPageLoad(function() {
+        $(document).ready(function(){
+
+            var hash = window.location.hash,
+                $target = false;
+
+            if (hash) {
+                hash = hash.split(':');
+                if (hash[0] == "#task") {
+                    $target = $('.tasks li[data-id='+hash[1]+']');
+                    if ($target.hasClass('complete')) {
+                        $('.show-completed-tasks').click();
+                    }
+                }
+                else if (hash[0] == "#comment") {
+                    $target = $('article.comment-row[data-id='+hash[1]+']');
+                }
+                else if (hash[0] == "#attachment") {
+                    $target = $('.file-list li[data-id='+hash[1]+']');
+                }
+
+                if ($target && $target.length) {
+                    $target.addClass('warmdown');
+
+                    $('html,body').animate({
+                      scrollTop: ($target.offset().top - 100)
+                    }, 300);
+                }
+            }
+
+        });
+    });
+});
