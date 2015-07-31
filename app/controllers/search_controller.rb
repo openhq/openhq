aa93d62d83d9ba69b5524f58d94e2c856d07dc06
Colin Young <hello@colin-young.co.uk>
2015-07-31 16:09:57 +0100

    use get request for the search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,18 +1,8 @@
 class SearchController < ApplicationController
 
-  def create
-    redirect_to search_path(search_params[:q].to_param)
-  end
-
-  def show
-    @query = params[:id]
+  def index
+    @query = params[:q]
     @results = PgSearch.multisearch(@query)
-  end
-
-  private
-
-  def search_params
-    params.require(:search).permit(:q)
   end
 
 end
