5f149373547e81bbb55fa904db06bc937f51f5f9
Colin Young <hello@colin-young.co.uk>
2015-08-11 15:34:42 +0100

    rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :projects, -> { order(name: :asc) }
 
   has_attached_file :avatar, styles: { thumb: "300x300#" }
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :avatar, content_type: %r{^image\/}
 
   validates :first_name, :last_name, presence: true
   validates :role, presence: true, inclusion: {in: ROLES}
