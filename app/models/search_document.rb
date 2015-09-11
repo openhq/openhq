b784aaf45a332bc1d44347b1396a30f467427318
Colin Young <hello@colin-young.co.uk>
2015-09-11 18:19:36 +0100

    somehow got the search working with that mental sql

--- app/models/search_document.rb
+++ app/models/search_document.rb
@@ -1,3 +1,9 @@
 class SearchDocument < ActiveRecord::Base
   belongs_to :searchable, polymorphic: true
+
+  def self.search(query, team_id)
+     where(team_id: team_id)
+    .joins('INNER JOIN (SELECT "search_documents"."id" AS search_id, (ts_rank((to_tsvector(\'english\', coalesce("search_documents"."content"::text, \'\'))), (to_tsquery(\'english\', \'\'\' \' || \'' + query + '\' || \' \'\'\' || \':*\')), 0)) AS rank FROM "search_documents" WHERE (((to_tsvector(\'english\', coalesce("search_documents"."content"::text, \'\'))) @@ (to_tsquery(\'english\', \'\'\' \' || \'' + query + '\' || \' \'\'\' || \':*\'))) OR ((coalesce("search_documents"."content"::text, \'\')) % \'' + query + '\')) ) search_search_documents ON "search_documents"."id" = search_search_documents.search_id')
+    .order('search_search_documents.rank DESC, updated_at DESC')
+  end
 end
