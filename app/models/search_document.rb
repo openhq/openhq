febc8a4fb495fe39fccb9a1afddf266109a2c46d
Pete Hawkins <pete@phawk.co.uk>
2015-09-14 11:16:15 +0100

    Escape sql for searches

--- app/models/search_document.rb
+++ app/models/search_document.rb
@@ -3,16 +3,20 @@ class SearchDocument < ActiveRecord::Base
 
   # cant quite believe that sql is working...
   def self.search(query, team_id)
-    where(team_id: team_id).joins('
+    query.gsub!(%r{[^a-z0-9\s]}, "")
+
+    sql = ActiveRecord::Base.sanitize_sql_array(['
       INNER JOIN (SELECT
         "search_documents"."id" AS search_id,
         (ts_rank((to_tsvector(\'english\', coalesce("search_documents"."content"::text, \'\'))),
-        (to_tsquery(\'english\', \'\'\' \' || \'' + query + '\' || \' \'\'\' || \':*\')), 0)) AS rank
+        (to_tsquery(\'english\', \'\'\' \' || :query || \' \'\'\' || \':*\')), 0)) AS rank
       FROM "search_documents"
       WHERE (((to_tsvector(\'english\', coalesce("search_documents"."content"::text, \'\'))) @@
-        (to_tsquery(\'english\', \'\'\' \' || \'' + query + '\' || \' \'\'\' || \':*\'))) OR
-        ((coalesce("search_documents"."content"::text, \'\')) % \'' + query + '\')) ) search_search_documents ON
+        (to_tsquery(\'english\', \'\'\' \' || :query || \' \'\'\' || \':*\'))) OR
+        ((coalesce("search_documents"."content"::text, \'\')) % :query)) ) search_search_documents ON
       "search_documents"."id" = search_search_documents.search_id
-    ').order('search_search_documents.rank DESC, updated_at DESC')
+    ', query: query])
+
+    where(team_id: team_id).joins(sql).order('search_search_documents.rank DESC, updated_at DESC')
   end
 end
