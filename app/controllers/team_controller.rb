c80367af9f6799bbabb15d855b6a11632f8a798a
Colin Young <hello@colin-young.co.uk>
2015-09-18 18:37:48 +0100

    ability to update users roles from the team pages

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -1,4 +1,6 @@
 class TeamController < ApplicationController
+  before_action :set_user_and_team_member, except: :index
+
   def index
     fresh_when current_team.users.maximum(:updated_at)
 
@@ -9,8 +11,22 @@ class TeamController < ApplicationController
   end
 
   def show
+    fresh_when last_modified: @team_member.updated_at
+  end
+
+  def update
+    @team_member.update(team_user_params)
+    redirect_to team_path(@user.username), notice: "User has been updated"
+  end
+
+  private
+
+  def set_user_and_team_member
     @user = current_team.users.find_by!(username: params[:id])
     @team_member = current_team.team_users.find_by!(user_id: @user.id)
-    fresh_when last_modified: @team_member.updated_at
+  end
+
+  def team_user_params
+    params.require(:team_user).permit(:role)
   end
 end
