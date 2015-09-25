dc1eb9f82a03c569f360d00ed073f05d8ddbc683
Pete Hawkins <pete@phawk.co.uk>
2015-09-25 17:51:13 +0100

    Close dialogs when clicking off

--- app/assets/javascripts/features/dropdowns.js
+++ app/assets/javascripts/features/dropdowns.js
@@ -1,4 +1,12 @@
 $(function() {
+    $(document).on("click", function() {
+        $(".ui-dropdown-menu.open").removeClass("open");
+    });
+
+    $(document).on("click", ".ui-dropdown-menu", function(ev) {
+        ev.stopPropagation();
+    });
+
     $(document).on("click", ".ui-dropdown-menu .icon", function() {
         var $menu = $(this).closest(".ui-dropdown-menu"),
             is_open = $menu.hasClass("open");
