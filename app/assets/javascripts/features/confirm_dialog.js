04c5e19607890ee6659edb2066a41397faeb5a46
Colin Young <hello@colin-young.co.uk>
2015-09-25 15:40:14 +0100

    styles and workings to show custom confirm dialog

--- app/assets/javascripts/features/confirm_dialog.js
+++ app/assets/javascripts/features/confirm_dialog.js
@@ -1,29 +1,32 @@
 $(function(){
-  // Override the default confirm dialog by rails
-  $.rails.allowAction = function(link){
-    if (link.data("confirm") == undefined){
-      return true;
-    }
-    $.rails.showConfirmationDialog(link);
-    return false;
-  }
-
-  // User click confirm button
-  $.rails.confirmed = function(link){
-    link.data("confirm", null);
-    link.trigger("click.rails");
-  }
-
-  // Display the confirmation dialog
-  $.rails.showConfirmationDialog = function(link){
-    var message = link.data("confirm");
-    $.fn.SimpleModal({
-      model: "modal",
-      title: "Please confirm",
-      contents: message
-    }).addButton("Confirm", "button alert", function(){
-      $.rails.confirmed(link);
-      this.hideModal();
-    }).addButton("Cancel", "button secondary").showModal();
-  }
+  $(document).on('click', '.confirm-dialog-wrapper .close-dialog', function(ev){
+    $(this).closest('.confirm-dialog-wrapper').fadeOut(100);
+  });
 });
+
+// Override the default confirm dialog by rails
+$.rails.allowAction = function(link){
+  if (link.data("confirm") == undefined){
+    return true;
+  }
+  $.rails.showConfirmationDialog(link);
+  return false;
+}
+
+// Display the confirmation dialog
+$.rails.showConfirmationDialog = function(link){
+  var message = link.data("confirm");
+  var $confirm_dialog = $('.confirm-dialog-wrapper');
+
+  $confirm_dialog.find('p.confirm-message').html(message);
+  $confirm_dialog.find('a.button.confirm').off().on('click', function(ev){
+    $.rails.confirmed(link);
+  });
+  $confirm_dialog.fadeIn(100);
+}
+
+// User click confirm button
+$.rails.confirmed = function(link){
+  link.data("confirm", null);
+  link.trigger("click.rails");
+}
