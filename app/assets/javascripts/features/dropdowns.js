9ba87ba670d5874923a53d4785cc6c9cf2a0e56c
Colin Young <hello@colin-young.co.uk>
2015-09-25 12:59:00 +0100

    notifications and search dropdowns

--- app/assets/javascripts/features/dropdowns.js
+++ app/assets/javascripts/features/dropdowns.js
@@ -1,7 +1,12 @@
 $(function() {
     $(document).on("click", ".ui-dropdown-menu .icon", function() {
-        var $menu = $(this).closest(".ui-dropdown-menu");
+        var $menu = $(this).closest(".ui-dropdown-menu"),
+            is_open = $menu.hasClass("open");
 
-        $menu.toggleClass("open");
+        $('.ui-dropdown-menu.open').removeClass('open');
+
+        if (!is_open) {
+          $menu.addClass('open');
+        }
     });
 });
