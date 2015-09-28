81630180ba940d9fc323aebfc9bcfbae1a10a6d2
Colin Young <hello@colin-young.co.uk>
2015-09-28 17:55:55 +0100

    paginate search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,11 +2,18 @@ class SearchController < ApplicationController
 
   def index
     @query = params[:q]
-    @results = SearchDocument.search(@query, current_team.id, current_user.project_ids).page((params[:page] || 1).to_i).per(20)
+    @results = SearchDocument.search(@query, current_team.id, current_user.project_ids).page((params[:page] || 1).to_i).per(1)
 
     respond_to do |format|
       format.html
-      format.json { render json: @results, meta: { total: @results.total_count } }
+      format.json do
+        render json: @results, meta: {
+          query: @query,
+          total: @results.total_count,
+          has_more: @results.total_count > @results.count,
+          view_more_link: search_index_path(q: @query, page: 2)
+        }
+      end
     end
   end
 
