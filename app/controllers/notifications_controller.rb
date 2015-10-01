c351da8e4dc896017b375917001549b94e5e77c2
Colin Young <hello@colin-young.co.uk>
2015-10-01 15:46:16 +0100

    adding the all notifications page

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,16 +1,14 @@
 class NotificationsController < ApplicationController
   def index
+    page = (params[:page] || 1).to_i
+    @notifications = current_user.notifications.page(page).per(20)
   end
 
   def unseen
-    return unless current_user.present?
-
     render json: current_user.notifications.unseen.reverse
   end
 
   def mark_all_seen
-    return unless current_user.present?
-
     current_user.notifications.unseen.each(&:mark_as_seen)
     render nothing: true
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   has_many :teams, through: :team_users
   has_many :created_projects, foreign_key: "owner_id", class_name: "Project"
   has_many :stories, foreign_key: "owner_id", class_name: "Story"
-  has_many :notifications, -> { order(created_at: :asc) }
+  has_many :notifications, -> { order(created_at: :desc) }
 
   has_and_belongs_to_many :projects, -> { order(name: :asc) }
 
