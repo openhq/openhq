f7133e325664d45df45553cfb2b3038c159e7b96
Pete Hawkins <pete@phawk.co.uk>
2015-10-13 12:40:45 +0100

    Styleguide issues

--- app/controllers/setup/multisite_controller.rb
+++ app/controllers/setup/multisite_controller.rb
@@ -1,27 +1,29 @@
-class Setup::MultisiteController < ApplicationController
-  layout "setup"
-  skip_before_action :require_login
-  skip_before_action :run_first_time_setup
+module Setup
+  class MultisiteController < ApplicationController
+    layout "setup"
+    skip_before_action :require_login
+    skip_before_action :run_first_time_setup
 
-  def index
-    # Ensure the setup code is valid
-    team = Team.find_by!(setup_code: params[:code])
+    def index
+      # Ensure the setup code is valid
+      team = Team.find_by!(setup_code: params[:code])
 
-    # Sign in the first and only user
-    sign_in team.users.first
-  end
-
-  def update_user
-    if current_user.update(user_params)
-      redirect_to setup_first_project_path
-    else
-      render :index
+      # Sign in the first and only user
+      sign_in team.users.first
     end
-  end
 
-  private
+    def update_user
+      if current_user.update(user_params)
+        redirect_to setup_first_project_path
+      else
+        render :index
+      end
+    end
 
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :username, :avatar)
+    private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :username, :avatar)
+    end
   end
 end
--- app/controllers/setup/single_site_controller.rb
+++ app/controllers/setup/single_site_controller.rb
@@ -1,38 +1,40 @@
-class Setup::SingleSiteController < ApplicationController
-  layout "setup"
-  skip_before_action :require_login
-  skip_before_action :run_first_time_setup
-  before_action :ensure_single_site_install, only: [:index, :create]
-  before_action :ensure_no_users, only: [:index, :create]
+module Setup
+  class SingleSiteController < ApplicationController
+    layout "setup"
+    skip_before_action :require_login
+    skip_before_action :run_first_time_setup
+    before_action :ensure_single_site_install, only: [:index, :create]
+    before_action :ensure_no_users, only: [:index, :create]
 
-  def index
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      current_team.team_users.create!(user: @user, role: "owner")
-      sign_in @user
-
-      redirect_to setup_first_project_path
-    else
-      render :first_time_user
+    def index
+      @user = User.new
     end
-  end
 
-  private
+    def create
+      @user = User.new(user_params)
 
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :username, :avatar)
-  end
+      if @user.save
+        current_team.team_users.create!(user: @user, role: "owner")
+        sign_in @user
 
-  def ensure_single_site_install
-    redirect_to root_url if multisite_install?
-  end
+        redirect_to setup_first_project_path
+      else
+        render :first_time_user
+      end
+    end
 
-  def ensure_no_users
-    redirect_to root_url if current_team.users.count > 0
+    private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :username, :avatar)
+    end
+
+    def ensure_single_site_install
+      redirect_to root_url if multisite_install?
+    end
+
+    def ensure_no_users
+      redirect_to root_url if current_team.users.count > 0
+    end
   end
 end
