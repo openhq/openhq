41f9dad77333c1dd10811bf35f95897e559b57d6
Colin Young <hello@colin-young.co.uk>
2015-10-14 17:22:08 +0100

    2 space indent

--- app/controllers/me_controller.rb
+++ app/controllers/me_controller.rb
@@ -1,10 +1,10 @@
 class MeController < ApplicationController
-    def index
-        tasks = current_user.tasks.incomplete.includes(:project, :story).order(due_at: :asc)
+  def index
+    tasks = current_user.tasks.incomplete.includes(:project, :story).order(due_at: :asc)
 
-        @overdue_tasks = tasks.where('due_at IS NOT NULL AND due_at < ?', Time.zone.now - 1.day)
-        @todays_tasks = tasks.where('due_at IS NOT NULL AND due_at > ? AND due_at < ?', Time.zone.now - 1.day, Time.zone.now)
-        @weeks_tasks = tasks.where('due_at IS NOT NULL AND due_at > ? AND due_at < ?', Time.zone.now, Time.zone.now + 6.days)
-        @other_tasks = tasks.where('due_at IS NULL OR due_at > ?', Time.zone.now + 6.days)
-    end
+    @overdue_tasks = tasks.where('due_at IS NOT NULL AND due_at < ?', Time.zone.now - 1.day)
+    @todays_tasks = tasks.where('due_at IS NOT NULL AND due_at > ? AND due_at < ?', Time.zone.now - 1.day, Time.zone.now)
+    @weeks_tasks = tasks.where('due_at IS NOT NULL AND due_at > ? AND due_at < ?', Time.zone.now, Time.zone.now + 6.days)
+    @other_tasks = tasks.where('due_at IS NULL OR due_at > ?', Time.zone.now + 6.days)
+  end
 end
