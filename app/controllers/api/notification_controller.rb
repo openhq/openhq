57e232654eb50707fd4efc9956e9b1a7836bff3b
Colin Young <hello@colin-young.co.uk>
2015-10-15 17:29:55 +0100

    refactor notifications api show

--- app/controllers/api/notification_controller.rb
+++ app/controllers/api/notification_controller.rb
@@ -2,12 +2,7 @@ module Api
   class NotificationController < ApplicationController
     def show
       n = current_user.notifications.find(params[:id])
-
-      if n.present?
-        render json: n, status: 200
-      else
-        render nothing: true, status: 400
-      end
+      render json: n, status: 200
     end
   end
 end
