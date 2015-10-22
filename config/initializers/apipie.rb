bf80f0766f76ffb37d44063ec19216826b7e8b5b
Pete Hawkins <pete@phawk.co.uk>
2015-10-22 15:49:40 +0100

    Update apipie app desc

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,5 +1,6 @@
 Apipie.configure do |config|
   config.app_name                = "Open HQ"
+  config.app_info                = "Open HQ API Docs"
   config.api_base_url            = "/api"
   config.doc_base_url            = "/api/docs"
   # where is your API defined?
