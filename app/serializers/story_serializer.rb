c5c4bdb749247e9337ae79f45d26e60c756cab7b
Pete Hawkins <pete@phawk.co.uk>
2015-10-23 11:44:12 +0100

    Update serializer embed style

--- app/serializers/story_serializer.rb
+++ app/serializers/story_serializer.rb
@@ -1,5 +1,4 @@
 class StorySerializer < ActiveModel::Serializer
-  embed :ids, embed_in_root: true
   attributes :id, :project_id, :team_id, :name, :slug, :description, :owner_id, :created_at, :updated_at, :deleted_at
   has_one :owner
 end
--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,13 +1,11 @@
 class TaskSerializer < ActiveModel::Serializer
-  attributes :id, :label, :assignment_name, :project, :story, :url, :project_users, :completed, :due_at, :due_at_pretty
+  attributes :id, :label, :assignment_name, :url, :owner_id, :completed, :due_at, :due_at_pretty
   has_one :assignment
+  has_one :story
+  has_one :project
 
   def url
     api_v1_task_path(id)
-  end
-
-  def project_users
-    project.users_select_array
   end
 
   def due_at_pretty
