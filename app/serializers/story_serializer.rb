e4940b3ece779a6b67238a821f684a67522e9461
Pete Hawkins <pete@phawk.co.uk>
2015-10-23 15:01:26 +0100

    remove the root name

--- app/serializers/story_serializer.rb
+++ app/serializers/story_serializer.rb
@@ -2,7 +2,7 @@ class StorySerializer < ActiveModel::Serializer
   include TagHelper
 
   attributes :id, :project_id, :team_id, :name, :slug, :description, :markdown, :owner_id, :created_at, :updated_at, :deleted_at
-  has_one :owner, embed: :id, embed_in_root: true, root: "users"
+  has_one :owner, embed: :id, embed_in_root: true
 
   def markdown
     markdownify(object.description)
