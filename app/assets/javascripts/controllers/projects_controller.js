3a572d0a1f886f620212d427189545ef26081825
Pete Hawkins <pete@phawk.co.uk>
2015-10-29 15:44:36 +0000

    create a new project

--- app/assets/javascripts/controllers/projects_controller.js
+++ app/assets/javascripts/controllers/projects_controller.js
@@ -1,3 +1,10 @@
 angular.module("OpenHq").controller("ProjectsController", function($scope, Project) {
+  $scope.newProject = {};
   $scope.projects = Project.getList().$object;
+
+  $scope.createProject = function(newProject) {
+    Project.post(newProject).then(function(project) {
+      $scope.projects.push(project);
+    });
+  };
 });
