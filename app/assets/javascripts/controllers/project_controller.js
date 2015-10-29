ae8e4434d03cab9d3efb6f5f48c83bca009524ec
Pete Hawkins <pete@phawk.co.uk>
2015-10-29 19:02:30 +0000

    fetch stories on their own

--- app/assets/javascripts/controllers/project_controller.js
+++ app/assets/javascripts/controllers/project_controller.js
@@ -1,5 +1,9 @@
-angular.module("OpenHq").controller("ProjectController", function($scope, $routeParams, Restangular) {
+angular.module("OpenHq").controller("ProjectController", function($scope, $routeParams, Restangular, storiesRepository) {
   Restangular.one("projects", $routeParams.slug).get().then(function(project) {
     $scope.project = project;
+  });
+
+  storiesRepository.getList({project_id: $routeParams.slug}).then(function(stories) {
+    $scope.stories = stories;
   });
 });
