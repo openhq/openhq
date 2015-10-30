14f93992b6b79260c533015a3b2a11d642a13eb1
Pete Hawkins <pete@phawk.co.uk>
2015-10-30 16:43:21 +0000

    remove dev logs

--- app/assets/javascripts/controllers/story_controller.js
+++ app/assets/javascripts/controllers/story_controller.js
@@ -11,14 +11,12 @@ angular.module("OpenHq").controller("StoryController", function($scope, $routePa
 
   $scope.createComment = function(newComment) {
     newComment.create().then(function(resp) {
-      console.log("comment created", resp);
       $scope.story.comments.push(resp);
     });
   }
 
   $scope.createTask = function(newTask) {
     newTask.create().then(function(resp) {
-      console.log("task created", resp);
       $scope.story.tasks.push(resp);
     });
   }
