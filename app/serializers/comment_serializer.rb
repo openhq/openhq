ed783d4f31caa4851adb258685c6a152fbe42f5a
Pete Hawkins <pete@phawk.co.uk>
2015-10-30 12:03:47 +0000

    eager load comment owners when fetching a single story

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,9 +1,11 @@
 class CommentSerializer < ActiveModel::Serializer
   include TagHelper
 
-  attributes :id, :content, :markdown, :commentable_type, :commentable_id, :owner_id, :created_at, :updated_at, :team_id
+  attributes :id, :content, :markdown, :commentable_type, :commentable_id, :owner_id, :created_at, :updated_at, :team_id, :owner
 
-  has_one :owner
+  def owner
+    ActiveModel::SerializableResource.new(object.owner).serializable_hash[:user]
+  end
 
   def markdown
     markdownify(object.content)
--- app/serializers/story_serializer.rb
+++ app/serializers/story_serializer.rb
@@ -4,7 +4,7 @@ class StorySerializer < ActiveModel::Serializer
   attributes :id, :project_id, :team_id, :owner_id, :name, :slug, :description, :markdown, :created_at, :updated_at, :deleted_at
   has_one :owner
   has_one :project
-  has_many :comments
+  has_many :comments, each_serializer: CommentSerializer
   has_many :tasks
 
   def markdown
