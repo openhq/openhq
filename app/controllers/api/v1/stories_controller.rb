a46993d90bebcdfcabde8e387b505ca59d6bedf6
Pete Hawkins <pete@phawk.co.uk>
2015-10-30 11:18:06 +0000

    preload a bunch of stuff in a story

--- app/controllers/api/v1/stories_controller.rb
+++ app/controllers/api/v1/stories_controller.rb
@@ -17,7 +17,7 @@ module Api
       api! "Fetch all stories"
       param :project_id, String, desc: "Project ID or slug", required: true
       def index
-        render json: @project.stories.includes(:owner)
+        render json: @project.stories.includes(:owner, :project, :comments, :tasks)
       end
 
       api! "Fetch a single story"
