b592fc875e7fbc6873b31eb5b96362035005bfa5
Pete Hawkins <pete@phawk.co.uk>
2015-10-30 11:06:16 +0000

    Add some validation specs to project model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Project do
   it "must be valid" do
     expect(project).to be_valid
   end
+
+  it { should belong_to :owner }
+  it { should have_many :stories }
+  it { should have_many :recent_stories }
+  it { should have_and_belong_to_many :users }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :owner_id }
 end
