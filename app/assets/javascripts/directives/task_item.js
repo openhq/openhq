b4d1e6ee423568452cdfd09fb6663287b56a32e9
Colin Young <hello@colin-young.co.uk>
2015-10-30 15:45:23 +0000

    show/hide the edit task form

--- app/assets/javascripts/directives/task_item.js
+++ app/assets/javascripts/directives/task_item.js
@@ -1,13 +1,33 @@
 angular.module("OpenHq").directive("taskItem", function() {
-    return {
-        restrict: "E",
-        scope: {
-          task: '=',
-          users: '=',
-        },
-        template: JST['templates/directives/task_item'],
-        controller: function($scope){
-            console.log($scope.users);
-        }
-    };
+  return {
+    restrict: "E",
+    scope: {
+      task: '=',
+      users: '=',
+    },
+    template: JST['templates/directives/task_item'],
+
+    controller: function($scope){
+      $scope.editing = false;
+
+      $scope.startEditing = function($event) {
+        $scope.editing = true;
+        $($event.currentTarget).closest('li').addClass('editing');
+      }
+
+      $scope.stopEditing = function($event) {
+        $scope.editing = false;
+        $($event.currentTarget).closest('li').removeClass('editing');
+      }
+
+      $scope.deleteTask = function($event) {
+        console.log('delete that thang');
+      }
+
+      $scope.updateTask = function($event) {
+        $event.preventDefault();
+        console.log('update thon task');
+      }
+    }
+  };
 });
