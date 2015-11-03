5d1e286e86b63d74c3160233b4820cea94d049d6
Pete Hawkins <pete@phawk.co.uk>
2015-11-03 14:44:21 +0000

    Allow selection of users when creating

--- app/assets/javascripts/controllers/projects_controller.js
+++ app/assets/javascripts/controllers/projects_controller.js
@@ -1,8 +1,18 @@
-angular.module("OpenHq").controller("ProjectsController", function($scope, Project, projects) {
+angular.module("OpenHq").controller("ProjectsController", function($scope, UsersRepository, Project, projects) {
   $scope.newProject = new Project();
+  $scope.newProjectUsers = {};
   $scope.projects = projects;
+  $scope.teamUsers = UsersRepository.all();
 
-  $scope.createProject = function(newProject) {
+  $scope.createProject = function(newProject, newProjectUsers) {
+    var selectedUsers = [];
+
+    _.each(newProjectUsers, function(selected, userId) {
+      if (selected) selectedUsers.push(userId);
+    });
+
+    newProject.user_ids = selectedUsers;
+
     newProject.create().then(function(project) {
       $scope.projects.push(project);
     });
