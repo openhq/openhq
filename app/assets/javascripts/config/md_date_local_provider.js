d98dff89582e94cf27ba837418ca1627dd7a1991
Colin Young <hello@colin-young.co.uk>
2015-11-06 15:03:23 +0000

    make sure dates are using js Date

--- app/assets/javascripts/config/md_date_local_provider.js
+++ app/assets/javascripts/config/md_date_local_provider.js
@@ -2,8 +2,4 @@ angular.module("OpenHq").config(function($mdDateLocaleProvider) {
   $mdDateLocaleProvider.formatDate = function(date) {
     return date ? moment(date).format('Do MMM, YYYY') : "";
   };
-  $mdDateLocaleProvider.parseDate = function(dateString) {
-    var m = moment(dateString, 'Do MMM, YYYY', true);
-    return m.isValid() ? m.toDate() : "";
-  };
 });
