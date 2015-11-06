af7a2216932c02d37492348c73d9457adad497bd
Colin Young <hello@colin-young.co.uk>
2015-11-06 17:27:39 +0000

    added confirm dialog to archive project

--- app/assets/javascripts/controllers/manage_project_controller.js
+++ app/assets/javascripts/controllers/manage_project_controller.js
@@ -1,15 +1,14 @@
-angular.module("OpenHq").controller("ManageProjectController", function($scope, $routeParams, $location, Project) {
+angular.module("OpenHq").controller("ManageProjectController", function($scope, $rootScope, $routeParams, $location, Project) {
   Project.get($routeParams.slug).then(function(project) {
     $scope.project = project;
   });
 
   $scope.archiveProject = function() {
-    var c = confirm("Are you sure you want to archive this project?");
-    if (!c) return;
-
-    $scope.project.delete().then(function(resp){
-      // TODO: add a notification
-      $location.url('/');
+    $rootScope.confirm('Archive Project', 'Are you sure?', function(){
+      $scope.project.delete().then(function(resp){
+        // TODO: add a notification
+        $location.url('/');
+      });
     });
   };
 });
