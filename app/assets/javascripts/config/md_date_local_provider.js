9467a1f26cc69cda5b20e13a63c127c9b37a198b
Colin Young <hello@colin-young.co.uk>
2015-11-06 14:41:36 +0000

    default to nothing if no date passed through

--- app/assets/javascripts/config/md_date_local_provider.js
+++ app/assets/javascripts/config/md_date_local_provider.js
@@ -1,5 +1,9 @@
 angular.module("OpenHq").config(function($mdDateLocaleProvider) {
   $mdDateLocaleProvider.formatDate = function(date) {
-    return moment(date).format('Do MMM, YYYY');
+    return date ? moment(date).format('Do MMM, YYYY') : "";
+  };
+  $mdDateLocaleProvider.parseDate = function(dateString) {
+    var m = moment(dateString, 'Do MMM, YYYY', true);
+    return m.isValid() ? m.toDate() : "";
   };
 });
