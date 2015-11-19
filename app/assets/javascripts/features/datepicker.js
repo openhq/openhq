c7c5c01d44d45a8fbc05b1d7519fbe391afa304f
Pete Hawkins <pete@phawk.co.uk>
2015-11-19 11:59:48 +0000

    Remove old JS features

--- app/assets/javascripts/features/datepicker.js
+++
@@ -1,9 +0,0 @@
-App.onPageLoad(function() {
-
-    $("input.datepicker").pickadate({
-        firstDay: 1,
-        formatSubmit: "yyyy-mm-dd",
-        hiddenName: true
-    });
-
-});
--- app/assets/javascripts/features/forms.js
+++
@@ -1,7 +0,0 @@
-App.onPageLoad(function() {
-
-    // Setup special select fields
-    $("select.chosen-select").chosen();
-    $("select[multiple]").chosen({no_results_text: "Oops, nothing found!"});
-
-});
