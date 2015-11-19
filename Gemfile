c07f1af21a3a75ca9d6f8b4aa68bdf2b4687da19
Pete Hawkins <pete@phawk.co.uk>
2015-11-19 21:48:12 +0000

    remove s3 direct upload gem

--- Gemfile
+++ Gemfile
@@ -42,7 +42,6 @@ gem 'friendly_id', '~> 5.0.4'
 # File uploads
 gem 'paperclip', '~> 4.2'
 gem 'aws-sdk', '< 2.0'
-gem 's3_direct_upload', '~> 0.1.7'
 
 # Background jobs
 gem 'sucker_punch'
--- config/initializers/s3_direct_upload.rb
+++
@@ -1,7 +0,0 @@
-S3DirectUpload.config do |c|
-  c.access_key_id = ENV['AWS_ACCESS_KEY_ID']
-  c.secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-  c.bucket = ENV['AWS_S3_BUCKET']
-  c.region = ENV['AWS_S3_REGION'] || nil
-  c.url = nil                # S3 API endpoint (optional), eg. "https://#{c.bucket}.s3.amazonaws.com/"
-end
