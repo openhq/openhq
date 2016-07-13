495c6d62b14f1cce3035521c435b66e74e5be5ee
Pete Hawkins <pete@phawk.co.uk>
2016-07-13 16:09:10 +0100

    Updates to fix tests

--- app/serializers/search_document_api_serializer.rb
+++ app/serializers/search_document_api_serializer.rb
@@ -4,37 +4,37 @@ class SearchDocumentApiSerializer < ActiveModel::Serializer
   attributes :searchable_id, :searchable_type, :searchable, :url, :project, :project_url, :story, :story_url, :attachment_image
 
   def project_url
-    project.present? ? api_v1_project_path(project) : nil
+    object.project.present? ? api_v1_project_path(object.project) : nil
   end
 
   def story_url
-    story.present? ? api_v1_story_path(story) : nil
+    object.story.present? ? api_v1_story_path(object.story) : nil
   end
 
   def url
-    case searchable_type
+    case object.searchable_type
     when "Project"
-      api_v1_project_path(searchable)
+      api_v1_project_path(object.searchable)
     when "Story"
-      api_v1_story_path(searchable)
+      api_v1_story_path(object.searchable)
     when "Task"
-      api_v1_task_path(searchable)
+      api_v1_task_path(object.searchable)
     when "Comment"
-      api_v1_comment_path(searchable)
+      api_v1_comment_path(object.searchable)
     when "Attachment"
-      api_v1_attachment_path(searchable)
+      api_v1_attachment_path(object.searchable)
     end
   end
 
   def attachment_image
-    return unless searchable_type == "Attachment"
+    return unless object.searchable_type == "Attachment"
 
-    thumb_url = searchable.process_data["thumbnail:tile"]
+    thumb_url = object.searchable.process_data["thumbnail:tile"]
 
     if thumb_url.present?
       S3UrlSigner.sign(thumb_url)
     else
-      ActionController::Base.helpers.image_url "file_types/#{searchable.icon_name}.png"
+      ActionController::Base.helpers.image_url "file_types/#{object.searchable.icon_name}.png"
     end
   end
 end
