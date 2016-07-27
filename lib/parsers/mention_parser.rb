d6466887a98f9328e15ba0f6981847529c67a6dd
Pete Hawkins <pete@phawk.co.uk>
2016-07-27 15:06:38 +0100

    allow hyphens and underscores in mentions

--- lib/parsers/mention_parser.rb
+++ lib/parsers/mention_parser.rb
@@ -1,6 +1,6 @@
 class MentionParser
   def self.users(content)
-    usernames = String(content).scan(/\B@\w+/).map { |u| u.gsub(/@/, '') }
+    usernames = String(content).scan(/\B@[0-9a-zA-Z\-_]/).map { |u| u.gsub(/@/, '') }
     User.where("username IN (?)", usernames)
   end
 end
